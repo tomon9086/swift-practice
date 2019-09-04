5fafc207f22257d1d1962152d0387e704763ac51
tomon9086 <tomo.mouse.9086@gmail.com>
2019-09-04 18:51:39 +0900

    move, rename

--- shell/modules/Accessable.swift
+++ shell/modules/Accessable.swift
@@ -1,3 +1,8 @@
 protocol Accessable {
+	var parent: Directory? { get set }
 	var name: String { get set }
+	var path: String { get set }
+
+	func move(to: Directory) -> Void
+	func rename(name: String) -> Void
 }
--- shell/modules/Directory.swift
+++ shell/modules/Directory.swift
@@ -1,10 +1,12 @@
 public class Directory: Accessable {
-	let parent: Directory?
+	private let isRoot: Bool
+	var parent: Directory?
 	var name: String
 	var path: String
 	var children: [String: Accessable] = [:]
 
 	init(name: String, parent: Directory?) {
+		self.isRoot = parent == nil
 		self.parent = parent
 		self.name = name
 		if let parentDirectory = parent {
@@ -15,5 +17,21 @@ public class Directory: Accessable {
 			children[".."] = self
 		}
 		children["."] = self
+	}
+
+	func move(to: Directory) {
+		if self.isRoot {
+			print("root directory can not be moved.")
+			return
+		}
+		self.parent = to
+		self.children[".."] = self.parent
+	}
+
+	func rename(name: String) {
+		let filteredChildlen = self.parent?.children.filter({ $0.1.name == self.name }) ?? []
+		print(filteredChildlen)
+		if filteredChildlen.count > 0 {
+		}
 	}
 }
